ea3fa78c84ffe91fdb33e27c2c1fb7c6f88e69c0
Bahodir <choriyevbahodir0>
2024-05-14 21:19:58 +0500

    some changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Brand from "./Brand/BrandPage";
-// import App from "./App.tsx";
+// import Brand from "./Brand/BrandPage";
+import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		{/* <App /> */}
-		<Brand />
+		<App />
+		{/* <Brand /> */}
 	</React.StrictMode>
 );
