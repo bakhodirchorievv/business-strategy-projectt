3deefa86659441cd2412a1fbda82db796e4c3ca2
Bahodir <choriyevbahodir0>
2024-05-30 13:29:05 +0500

    styles conflict resolved

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,34 @@
-import { useEffect } from "react";
+import { lazy, Suspense, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
-import MmainPage from "./Components/Mmain/MmainPage";
-import Cases from "./Components/Cases/Cases";
-import BrandStrategy from "./Components/BrandStrategy/BrandStrategy";
-import Card from "./Components/Card/Card";
-import Contact from "./Components/Contacts/Contact";
-import Corporate from "./Components/Corporate/Corporate";
-import Corporative from "./Components/Corporative/Corporative";
-import Descriptor from "./Components/Descriptor/Descriptor";
-import Expert from "./Components/Expert/Expert";
-import Guideline from "./Components/Guideline/Guideline";
-import Logo from "./Components/Logo/Logo";
-import MDesign from "./Components/MDesign/MDesign";
-import Naming from "./Components/Naming/Naming";
-import Native from "./Components/Native/Native";
-import Packing from "./Components/Packing/Packing";
-import Partner from "./Components/Partner/Partner";
-import Presentation from "./Components/Presentatiton/Presentation";
-import Radio from "./Components/Radio/Radio";
-import Shop from "./Components/Shop/Shop";
-import Site from "./Components/Site/Site";
-import Souvenir from "./Components/Souvenir/Souvenir";
-import Television from "./Components/Television/Television";
+
+const MmainPage = lazy(() => import("./Components/Mmain/MmainPage"));
+const Cases = lazy(() => import("./Components/Cases/Cases"));
+const BrandStrategy = lazy(
+	() => import("./Components/BrandStrategy/BrandStrategy")
+);
+const Card = lazy(() => import("./Components/Card/Card"));
+const Contact = lazy(() => import("./Components/Contacts/Contact"));
+const Corporate = lazy(() => import("./Components/Corporate/Corporate"));
+const Corporative = lazy(() => import("./Components/Corporative/Corporative"));
+const Descriptor = lazy(() => import("./Components/Descriptor/Descriptor"));
+const Expert = lazy(() => import("./Components/Expert/Expert"));
+const Guideline = lazy(() => import("./Components/Guideline/Guideline"));
+const Logo = lazy(() => import("./Components/Logo/Logo"));
+const MDesign = lazy(() => import("./Components/MDesign/MDesign"));
+const Naming = lazy(() => import("./Components/Naming/Naming"));
+const Native = lazy(() => import("./Components/Native/Native"));
+const Packing = lazy(() => import("./Components/Packing/Packing"));
+const Partner = lazy(() => import("./Components/Partner/Partner"));
+const Presentation = lazy(
+	() => import("./Components/Presentatiton/Presentation")
+);
+const Radio = lazy(() => import("./Components/Radio/Radio"));
+const Shop = lazy(() => import("./Components/Shop/Shop"));
+const Site = lazy(() => import("./Components/Site/Site"));
+const Souvenir = lazy(() => import("./Components/Souvenir/Souvenir"));
+const Television = lazy(() => import("./Components/Television/Television"));
 
 const App = () => {
 	useEffect(() => {
@@ -34,30 +39,32 @@ const App = () => {
 		<Router basename="/business-strategy-project">
 			<div>
 				<Header />
-				<Routes>
-					<Route path="/" element={<MmainPage />} />
-					<Route path="/Cases" element={<Cases />} />
-					<Route path="/BrandStrategy" element={<BrandStrategy />} />
-					<Route path="/Card" element={<Card />} />
-					<Route path="/Contact" element={<Contact />} />
-					<Route path="/Corporate" element={<Corporate />} />
-					<Route path="/Corporative" element={<Corporative />} />
-					<Route path="/Descriptor" element={<Descriptor />} />
-					<Route path="/Expert" element={<Expert />} />
-					<Route path="/Guideline" element={<Guideline />} />
-					<Route path="/Logo" element={<Logo />} />
-					<Route path="/MDesign" element={<MDesign />} />
-					<Route path="/Naming" element={<Naming />} />
-					<Route path="/Native" element={<Native />} />
-					<Route path="/Packing" element={<Packing />} />
-					<Route path="/Partner" element={<Partner />} />
-					<Route path="/Presentation" element={<Presentation />} />
-					<Route path="/Radio" element={<Radio />} />
-					<Route path="/Shop" element={<Shop />} />
-					<Route path="/Site" element={<Site />} />
-					<Route path="/Souvenir" element={<Souvenir />} />
-					<Route path="/Television" element={<Television />} />
-				</Routes>
+				<Suspense fallback={<div>Loading...</div>}>
+					<Routes>
+						<Route path="/" element={<MmainPage />} />
+						<Route path="/Cases" element={<Cases />} />
+						<Route path="/BrandStrategy" element={<BrandStrategy />} />
+						<Route path="/Card" element={<Card />} />
+						<Route path="/Contact" element={<Contact />} />
+						<Route path="/Corporate" element={<Corporate />} />
+						<Route path="/Corporative" element={<Corporative />} />
+						<Route path="/Descriptor" element={<Descriptor />} />
+						<Route path="/Expert" element={<Expert />} />
+						<Route path="/Guideline" element={<Guideline />} />
+						<Route path="/Logo" element={<Logo />} />
+						<Route path="/MDesign" element={<MDesign />} />
+						<Route path="/Naming" element={<Naming />} />
+						<Route path="/Native" element={<Native />} />
+						<Route path="/Packing" element={<Packing />} />
+						<Route path="/Partner" element={<Partner />} />
+						<Route path="/Presentation" element={<Presentation />} />
+						<Route path="/Radio" element={<Radio />} />
+						<Route path="/Shop" element={<Shop />} />
+						<Route path="/Site" element={<Site />} />
+						<Route path="/Souvenir" element={<Souvenir />} />
+						<Route path="/Television" element={<Television />} />
+					</Routes>
+				</Suspense>
 				<Footer />
 			</div>
 		</Router>
