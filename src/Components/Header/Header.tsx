86a2d579486adca9928462750f9e33cce27e4983
Bahodir <choriyevbahodir0>
2024-07-08 20:25:11 +0500

    responsive nav bar added

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -32,6 +32,56 @@ const Header = () => {
 		};
 	}, []);
 
+	useEffect(() => {
+		const overlay = document.querySelector(".overlay");
+		const navBarLis = document.querySelectorAll(".nav-bar-li");
+
+		const handleNavItemClick = () => {
+			const navBar = document.querySelector(".nav-bar-res");
+			navBar?.classList.remove("appear");
+			navBar?.classList.add("disappear");
+			overlay?.classList.remove("block");
+			document.body.classList.remove("overFlowHide");
+		};
+
+		navBarLis.forEach((li) => {
+			li.addEventListener("click", handleNavItemClick);
+		});
+
+		return () => {
+			navBarLis.forEach((li) => {
+				li.removeEventListener("click", handleNavItemClick);
+			});
+		};
+	}, []);
+
+	const handleClickX = () => {
+		const navBar = document.querySelector(".nav-bar-res");
+		const overlay = document.querySelector(".overlay");
+		navBar?.classList.remove("appear");
+		navBar?.classList.add("disappear");
+		overlay?.classList.remove("block");
+		document.body.classList.remove("overFlowHide");
+	};
+
+	const handleClickAppear = () => {
+		const navBar = document.querySelector(".nav-bar-res");
+		const overlay = document.querySelector(".overlay");
+		navBar?.classList.remove("disappear");
+		navBar?.classList.add("appear");
+		overlay?.classList.add("block");
+		document.body.classList.add("overFlowHide");
+	};
+
+	const handleOverlay = () => {
+		const navBar = document.querySelector(".nav-bar-res");
+		const overlay = document.querySelector(".overlay");
+		navBar?.classList.remove("appear");
+		navBar?.classList.add("disappear");
+		overlay?.classList.remove("block");
+		document.body.classList.remove("overFlowHide");
+	};
+
 	return (
 		<>
 			<header id="real-main-header" ref={headerRef} className="header">
@@ -57,12 +107,36 @@ const Header = () => {
 						время написать
 					</button>
 					<img
+						onClick={handleClickAppear}
 						src="/business-strategy-project/MainPage/realBurger.png"
 						alt=""
 						className="getBlock"
 					/>
 				</div>
 			</header>
+
+			<div className="nav-bar-res">
+				<h2 onClick={handleClickX} className="xButton">
+					×
+				</h2>
+				<img
+					src="/business-strategy-project/MainPage/Vector (4).png"
+					alt=""
+					className="nav-bar-img"
+				/>
+
+				<ul className="nav-bar-ul">
+					<Link to={"Cases"}>
+						<li className="nav-bar-li">Кейсы</li>
+					</Link>
+					<li className="nav-bar-li">Услуги</li>
+					<li className="nav-bar-li">О компании</li>
+					<Link to={"Contact"}>
+						<li className="nav-bar-li">Контакты</li>
+					</Link>
+				</ul>
+			</div>
+			<div onClick={handleOverlay} className="overlay"></div>
 		</>
 	);
 };
